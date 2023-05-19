bfa6550873c412629a881df178ff11d43609ca62
Alif Triadi <aliftriadi.gaming@gmail.com>
2023-05-19 23:56:10 +0700

    Add: Working with PPDB and Landing page

--- app/Http/Controllers/Api/StudentCandidate/StoreStudentCandidateController.php
+++ app/Http/Controllers/Api/StudentCandidate/StoreStudentCandidateController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api\StudentCandidate;
 
+use App\Actions\AcademicYear\FetchActiveAcademicYearAction;
 use App\Actions\StudentCandidate\StoreStudentCandidateAction;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StudentCandidate\StoreStudentCandidateRequest;
@@ -10,6 +11,9 @@ class StoreStudentCandidateController extends Controller
 {
     public function __invoke(StoreStudentCandidateRequest $request)
     {
+        $academicYear = FetchActiveAcademicYearAction::resolve()->execute();
+        if (!$academicYear->ppdb) return $this->resolveForFailureResponseWith('PPDB is not open yet.', [], 403);
+
         StoreStudentCandidateAction::resolve()->execute($request);
 
         return $this->resolveForSuccessResponseWith('Successful registration, please wait for further information.', [], 201);
